a517c06c62a3142d14137b3f531b78896d1e50f5
Cormac Maher <G00463317@atu.ie>
2025-11-13 12:25:47 +0000

    Added jso n

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,26 +1,57 @@
-﻿namespace MovieExplorer
+﻿using Microsoft.Maui.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MovieExplorer
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         int i = 0;
-        int j = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
+  
+        private void EnterName_Clicked(object? sender, EventArgs e)
+        {            
+            SubmitBtn1.SetValue(Button.IsEnabledProperty, false);
+            OnStart();
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void OnStart()
+        {
+            CreateTheGrid();
+        }
+        private void CreateTheGrid()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GridPageContent.AddRowDefinition(new RowDefinition());
+                GridPageContent.AddColumnDefinition(new ColumnDefinition());
+
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Border styledBorder = new Border
+                    {
+                        BackgroundColor = Colors.Red,
+                        Stroke = Colors.Black,
+                        StrokeThickness = 3
+                    };
+                    GridPageContent.Add(styledBorder, i, j);
+
+                    TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+  
+                    styledBorder.GestureRecognizers.Add(tapGestureRecognizer);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
     }
 }
