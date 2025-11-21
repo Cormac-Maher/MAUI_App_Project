6f260dc41ed29724fe6ffe76149e7debb226dd12
Cormac Maher <G00463317@atu.ie>
2025-11-21 12:45:57 +0000

    Added movies c# page

---
+++ Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieExplorer
+{
+    internal class Movies
+    {
+        public string title { get; set; }
+        public int year { get; set; }
+        public List<string> genre { get; set; }
+        public string director { get; set; }
+        public double rating { get; set; }
+        public string emoji { get; set; }
+    }
+}
